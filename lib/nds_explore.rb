c21f093c74d65177b4376ee194fa6adae522833b
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-27 13:50:56 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,6 +9,8 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  
   row_index = 0
-while row_index < vm.length do
+while row_index < directors_database.length do
+  puts ""
 end
